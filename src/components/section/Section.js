7343483dc88b9dbce6c8812382c37bcd6e4e2c52
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-03-03 12:27:48 +0100

    refactoring of initial resources started. tbc

---
+++ src/components/section/Section.js
@@ -0,0 +1,15 @@
+import SearchForm from "../search/SearchForm";
+
+const Section = () => {
+    return(
+        <section className="card users-container">
+           {/* Search bar componen - search form*/ } 
+            <SearchForm />
+
+            {/* Table component */ } 
+            
+        </section>
+    )
+}
+
+export default Section;
