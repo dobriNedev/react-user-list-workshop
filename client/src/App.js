f6bc0534ca5a3e330ae16a8fa2d6312706cc3754
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-03-06 13:15:12 +0100

    initial components created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-
-//import './App.css';
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Pagination from './components/Pagination';
+import Search from './components/Search';
+import UsersTable from './components/UsersTable';
 
 function App() {
   return (
     <>
-      
+      <Header />
+        <main className="main">
+          <section className="card users-container">
+            <Search />
+            <div className="table-wrapper">
+              <UsersTable />
+              <button className="btn-add btn">Add new user</button>
+              <Pagination />
+            </div>
+          </section>
+        </main> 
+      <Footer />
     </>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return(
+        <header className="header">
+        <div className="logo">
+          <span className="course">React Course - February 2023</span>
+          <span className="description">User List Workshop</span>
+        </div>
+      </header>
+    );
+};
+
+export default Header;
